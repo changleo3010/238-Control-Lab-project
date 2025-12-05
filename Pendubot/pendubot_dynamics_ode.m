fc37aac3ffa01fb0341ca77afff9ca3e25c635d2
Tianyi (Bruce) Chen HW account <96473587+gasaiginko@users.noreply.github.com>
2025-12-05 12:19:20 -0800

    Update pendubot_dynamics_ode.m

--- Pendubot/pendubot_dynamics_ode.m
+++ Pendubot/pendubot_dynamics_ode.m
@@ -5,6 +5,7 @@ function dx = pendubot_dynamics_ode(t, x, soln)
 u = soln.interp.control(t);
 u = u(1);   % scalar
 
-dx = pendubot_dynamics(t, x, u);   % 利用同一套 dynamics，只是 1 列而已
-dx = dx(:);                        % 保證是 4x1
+dx = pendubot_dynamics(t, x, u);   
+dx = dx(:);                        
 end
+
